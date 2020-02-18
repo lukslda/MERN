f6a9cfab14325ba7feeeeb36bdad08bffe5caaf0
lukslda <lukslda@gmail.com>
2020-02-18 19:16:11 -0300

    25.2 renombrando carpeta autho por auth, añadiendo alertas, funcionando correctamente

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Login from './components/autho/Login';
-import NuevaCuenta from './components/autho/NuevaCuenta';
+import Login from './components/auth/Login';
+import NuevaCuenta from './components/auth/NuevaCuenta';
 import Proyectos from './components/proyectos/Proyectos';
 
 import ProyectoState from './context/proyectos/proyectoState';
 import TareaState from "./context/tareas/tareaState";
+import AlertaState from "./context/alertas/alertaState";
 
 
 function App() {
   return (
     <ProyectoState>
       <TareaState>
-        <Router>
-          {/*todo lo que se encuentre en el router es lo que se van a ver en todas las paginas*/}
-          <Switch>
-            {/* todo lo que este adentro del switch seran las diferenteas paginas */}
-            <Route exact path="/" component={Login} />
-            <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-            <Route exact path="/proyectos" component={Proyectos} />
-          </Switch>
-        </Router>
+        <AlertaState>
+          <Router>
+            {/*todo lo que se encuentre en el router es lo que se van a ver en todas las paginas*/}
+            <Switch>
+              {/* todo lo que este adentro del switch seran las diferenteas paginas */}
+              <Route exact path="/" component={Login} />
+              <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+              <Route exact path="/proyectos" component={Proyectos} />
+            </Switch>
+          </Router>
+        </AlertaState>
       </TareaState>
     </ProyectoState>
   );
